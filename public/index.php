8618fe03648dfcf695108cf481a47744498d37fe
Vadim <cojocaru.vadim@gmail.com>
2017-07-24 22:52:31 +0300

    m

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ include "../vendor/autoload.php";
 use Twitter\Search\Search;
 
 $search = new Search();
-$search->setToken("B2PIACMAuMrjvmHd7pBVoX2WI","3Ukc2ZiH6glxM61l5Yp6C1vpr61HVxiS7Jzmnen8unomOTvIpY");
+$search->setToken();
 $value = ["q" => "ExtremeSetup"];
 echo "<pre>";
 print_r($search->search($value));
